244b4181b81ecf2ebf01f9eed575caef958408ae
olyamaksymchuk <109147843+olyamaksymchuk@users.noreply.github.com>
2023-02-14 16:39:30 +0200

    uploaded

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+
+const galleryContainerMarkup = 
+    galleryItems.map(({ preview, original, description }) => {
+         `
+    <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>
+
+`;
+    }).join("");
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryContainerMarkup);
